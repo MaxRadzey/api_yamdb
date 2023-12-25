9a53154e48c22121eba143f98eb9d7bdbf8756d5
Alexey Danilin <aleksey.danilin@gmail.com>
2023-12-25 22:40:25 +0300

    Changes to be committed: 	modified:   api_yamdb/api_yamdb/settings.py 	new file:   api_yamdb/users/__init__.py 	new file:   api_yamdb/users/admin.py 	new file:   api_yamdb/users/apps.py 	new file:   api_yamdb/users/migrations/0001_initial.py 	new file:   api_yamdb/users/migrations/__init__.py 	new file:   api_yamdb/users/models.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Users
+AUTH_USER_MODEL = 'users.DBUser'
 
 
 # Internationalization
