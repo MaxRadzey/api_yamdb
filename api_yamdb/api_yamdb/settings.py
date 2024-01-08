0c49bcb30c7971e24510af44f5bc89fd4a4671e7
Alexey Danilin <aleksey.danilin@gmail.com>
2024-01-08 09:49:17 +0000

    Changes to be committed: 	modified:   api_yamdb/api_yamdb/settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_rename_app",
     'rest_framework',
     'django_filters',
     'rest_framework.authtoken',
